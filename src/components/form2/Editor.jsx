8cc7725bb1a0e4d1ff4a2a3d638b8ef2e44a2a2b
AliTaee <ali.taee.72@gmail.com>
2021-11-01 10:55:11 +0330

    add unit test for form2

---
+++ src/components/form2/Editor.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import { savePost } from '../../api'
+
+function Editor({ user }) {
+  const [isSaving, setIsSaving] = useState(false)
+
+  function handleOnSubmit(e) {
+    e.preventDefault()
+    setIsSaving(true)
+    const { title, content, tags } = e.target.elements
+    savePost({
+      title: title.value,
+      content: content.value,
+      tags: tags.value.split(',').map((t) => t.trim()),
+      userId: user.id,
+    })
+  }
+
+  return (
+    <form onSubmit={handleOnSubmit}>
+      <label htmlFor="title-input">Title</label>
+      <input id="title-input" name="title" />
+
+      <label htmlFor="content-input">Content</label>
+      <textarea id="content-input" name="content" />
+
+      <label htmlFor="tags-input">Tags</label>
+      <input id="tags-input" name="tags" />
+
+      <button type="submit" disabled={isSaving}>
+        Submit
+      </button>
+    </form>
+  )
+}
+
+export { Editor }
---
+++ src/components/form2/Editor.test.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import userEvent from '@testing-library/user-event'
+import { render, screen } from '@testing-library/react'
+
+import { savePost as mockSavePost } from '../../api'
+import { Editor } from './Editor'
+
+jest.mock('../../api')
+
+afterEach(() => {
+  jest.clearAllMocks()
+})
+
+const fakePost = {
+  title: 'test title',
+  content: 'test content',
+  tags: ['tag1', 'tag2'],
+}
+
+const fakeUser = { id: 'user1' }
+
+test('renders a form with title, content, tags and submit button', () => {
+  mockSavePost.mockResolvedValueOnce()
+  render(<Editor user={fakeUser} />)
+
+  const inputTitle = screen.getByLabelText(/title/i)
+  userEvent.type(inputTitle, fakePost.title)
+
+  const inputContent = screen.getByLabelText(/content/i)
+  userEvent.type(inputContent, fakePost.content)
+
+  const inputTags = screen.getByLabelText(/tags/i)
+  userEvent.type(inputTags, fakePost.tags.join(','))
+
+  const submitButton = screen.getByText(/submit/i)
+  userEvent.click(submitButton)
+  expect(submitButton).toBeDisabled()
+
+  expect(mockSavePost).toHaveBeenCalledWith({
+    ...fakePost,
+    userId: fakeUser.id,
+  })
+  expect(mockSavePost).toHaveBeenCalledTimes(1)
+})
