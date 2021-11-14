a7776dcf0a1370ab8bf009e6fd9935913c49503b
AliTaee <ali.taee.72@gmail.com>
2021-11-14 22:46:43 +0330

    add test for initial redux

--- src/redux/redux-counter.test.js
+++ src/redux/redux-counter.test.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import { reducer } from './redux-reducer'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import { store } from './redux-store'
+import { store as appStore } from './redux-store'
 import { Counter } from './redux-counter'
 
 test('can render with redux with defaults', () => {
   render(
-    <Provider store={store}>
+    <Provider store={appStore}>
       <Counter />
     </Provider>,
   )
@@ -18,4 +20,18 @@ test('can render with redux with defaults', () => {
 
   userEvent.click(screen.getByText('-'))
   expect(countElement).toHaveTextContent(0)
+})
+
+test('can render with redux custom initial state', () => {
+  const store = createStore(reducer, { count: 3 })
+
+  render(
+    <Provider store={store}>
+      <Counter />
+    </Provider>,
+  )
+
+  const countElement = screen.getByLabelText('count')
+  userEvent.click(screen.getByText('+'))
+  expect(countElement).toHaveTextContent(4)
 })
