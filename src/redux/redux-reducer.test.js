061137458a157264720401e3c5fd7de96a769f27
AliTaee <ali.taee.72@gmail.com>
2021-11-18 10:10:59 +0330

    update reducer count tests

--- src/redux/redux-reducer.test.js
+++ src/redux/redux-reducer.test.js
@@ -1,18 +1,28 @@
 import { reducer } from './redux-reducer'
 import * as actionTypes from './actionTypes'
 
-test('Should render with default reducer', () => {
-  expect(reducer(undefined, {})).toEqual({ count: 0 })
-})
+describe('test reducer counter', () => {
+  test('Should render with default reducer', () => {
+    expect(reducer(undefined, {})).toEqual({ count: 0 })
+  })
 
-test('initial state in reducer', () => {
-  expect(reducer({ count: 10 }, {})).toEqual({ count: 10 })
-})
+  test('initial state in reducer', () => {
+    expect(reducer({ count: 10 }, {})).toEqual({ count: 10 })
+  })
 
-test('Should update count with increment action', () => {
-  expect(reducer(undefined, { type: actionTypes.INCREMENT })).toEqual({ count: 1 })
-})
+  test('Should update count with increment action', () => {
+    expect(reducer(undefined, { type: actionTypes.INCREMENT })).toEqual({ count: 1 })
+  })
 
-test('Should update count with decrement action', () => {
-  expect(reducer(undefined, { type: actionTypes.DECREMENT })).toEqual({ count: -1 })
+  test('Should update initial count with increment action', () => {
+    expect(reducer({ count: 9 }, { type: actionTypes.INCREMENT })).toEqual({
+      count: 10,
+    })
+  })
+
+  test('Should update count with decrement action', () => {
+    expect(reducer(undefined, { type: actionTypes.DECREMENT })).toEqual({
+      count: -1,
+    })
+  })
 })
