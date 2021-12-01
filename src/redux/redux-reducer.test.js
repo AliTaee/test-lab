fbad7f27303ea2decfac102b1061592003b1c968
AliTaee <ali.taee.72@gmail.com>
2021-12-01 17:10:37 +0330

    update reducer test case for redux

--- src/redux/redux-reducer.test.js
+++ src/redux/redux-reducer.test.js
@@ -1,28 +1,42 @@
 import { reducer } from './redux-reducer'
 import * as actionTypes from './actionTypes'
 
-describe('test reducer counter', () => {
-  test('Should render with default reducer', () => {
+describe('testing reducer', () => {
+  test('should return default value currently', () => {
+    expect(reducer({ count: 999 }, {})).toEqual({ count: 999 })
+  })
+
+  test('should return initial value currently', () => {
     expect(reducer(undefined, {})).toEqual({ count: 0 })
   })
 
-  test('initial state in reducer', () => {
-    expect(reducer({ count: 10 }, {})).toEqual({ count: 10 })
-  })
-
-  test('Should update count with increment action', () => {
-    expect(reducer(undefined, { type: actionTypes.INCREMENT })).toEqual({ count: 1 })
-  })
-
-  test('Should update initial count with increment action', () => {
-    expect(reducer({ count: 9 }, { type: actionTypes.INCREMENT })).toEqual({
-      count: 10,
+  test('should test INCREMENT action', () => {
+    expect(reducer(undefined, { type: actionTypes.INCREMENT })).toEqual({
+      count: 1,
     })
   })
 
-  test('Should update count with decrement action', () => {
+  test('should test DECREMENT action', () => {
     expect(reducer(undefined, { type: actionTypes.DECREMENT })).toEqual({
       count: -1,
+    })
+  })
+
+  test('should test wrong action type', () => {
+    expect(reducer(undefined, { type: 'wrongType' })).toEqual({
+      count: 0,
+    })
+  })
+
+  test('should test INCREMENT action with initial value', () => {
+    expect(reducer({ count: 10 }, { type: actionTypes.INCREMENT })).toEqual({
+      count: 11,
+    })
+  })
+
+  test('should test DECREMENT action with initial value', () => {
+    expect(reducer({ count: 10 }, { type: actionTypes.DECREMENT })).toEqual({
+      count: 9,
     })
   })
 })
